813f24fe2afa5062c3b61c12b53abb89a8a3b7c4
RaduAbde <raduabde@gmail.com>
2022-11-13 15:26:37 +0100

    Primera Version

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public String veredicto(String valor1, String valor2){
+        if (valor1.equals(valor2)) {
+            return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+            return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+        }
+        return "Ideal. Trabajas los contenidos en casa.";
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Main main = new Main();
+
+        System.out.println("Estudiante " + args[0] + " : ");
+        System.out.println(main.veredicto(args[1], args[2]));
+    }
+
+}
